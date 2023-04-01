5c921b95c88a04c9c68a8b3387d892282c848954
Muhammad Ayoub Khan <ayoubkhan558@hotmail.com>
2023-04-01 23:26:08 +0500

    fix

--- src/Components/Shared/Header.jsx
+++ src/Components/Shared/Header.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
@@ -17,22 +16,26 @@ function Header(props) {
   return (
     <header>
       <Navbar variant="dark" expand="lg">
-        <Container>
+        <div className="container">
           <Navbar.Brand href="#home">
             <HeaderLogo />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              {menuItems.map((menuItem) => (
+              {menuItems.map((menuItem, index) => (
                 <>
                   {menuItem.dropdown ? (
-                    <NavDropdown title={menuItem.label} id="basic-nav-dropdown">
-                      {menuItem.dropdown.map((dropdownItem) => (
+                    <NavDropdown
+                      title={menuItem.label}
+                      key={index}
+                      id="basic-nav-dropdown"
+                    >
+                      {menuItem.dropdown.map((dropdownItem, index) => (
                         // <NavDropdown.Divider />
                         <NavDropdown.Item
                           as={Link}
-                          key={dropdownItem.id}
+                          key={index}
                           to={dropdownItem.url}
                         >
                           {dropdownItem.label}
@@ -61,7 +64,7 @@ function Header(props) {
               ))}
             </Nav>
           </Navbar.Collapse>
-        </Container>
+        </div>
       </Navbar>
     </header>
   );
